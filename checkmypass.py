22f86d722df16c22d12524bd8a33d8b34e775770
Boitumelo Iven Moshitwa <itumelo553@gmail.com>
2024-03-09 14:06:47 +0200

    finish the project

--- checkmypass.py
+++ checkmypass.py
@@ -5,13 +5,13 @@ import hashlib
 
 
 def request_api_data(query_char):
-    url = 'https://api.pwnedpasswords.com/range/' + 'CBFDA'
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
     res = requests.get(url)
     if res.status_code != 200:
         raise RuntimeError(f"Error fetching:{res.status_code},check the api and try again")
     return res
 
-
+ 
 def get_password_leaks_count(hashes, hash_to_check):
     hashes = (line.split(":") for line in hashes.text.splitlines())
     for h, count in hashes:
@@ -20,12 +20,12 @@ def get_password_leaks_count(hashes, hash_to_check):
     return 0
 
 
-
 def pwned_api_check(password):
-    shalpassword = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
-    first5_char, tail = shalpassword[:5], shalpassword[5:]
+    sha1password = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
     res = request_api_data(first5_char)
     return get_password_leaks_count(res, tail)
+
 
 def main(args):
     for password in args:
@@ -34,6 +34,7 @@ def main(args):
             print(f"{password} was found {count} times. You should Prolly Change it")
         else:
             print(f"{password} was NOT found. Carry on!")
+    return "Done!"
 
 
 main(sys.argv[1:])
